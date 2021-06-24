7437b03c2b8c6983214b19659dcdc80fa3f176c8
Ojok Simon Peter <simonpeter.oj@gmail.com>
2021-06-24 12:25:31 -0700

    A function that prints an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,102 +1,46 @@
 #include "holberton.h"
 
 /**
- *powB - raises the number base to power power
- *@base : the base
- *@power : the power
- *Return: return the answer
- */
-int powB(unsigned int base, int power)
-{
-	int i, prod = 1;
-
-	for (i = 0; i < power; i++)
-	{
-		prod = prod * base;
-	}
-	return (prod);
-}
-
-/**
- *numLength - returns the lenth of string
- *@num : operand number
- *Return: number of digits
- */
-int numLength(unsigned int num)
-{
-	int length = 0;
-
-	if (!num)
-		return (1);
-
-	while (num)
-	{
-		num = num / 10;
-		length += 1;
-	}
-
-	return (length);
-}
-
-/**
- *printnchar - Print a number of any digit
- *@n : takes an input number
+ * print_number - prints an integer using putchar
+ * @n: integer to print
  *
- *
- */
-void printnchar(int n)
-{
-	int length, j, k, digit1, tmp2;
-	unsigned int num;
-
-	if (n < 0)
-	{
-		n *= -1;
-		_putchar('-');
-	}
-	num = n;
-	length = numLength(num), j = length - 1, k, digit1, tmp2;
-	if (num == 0)
-		_putchar(48);
-	else
-	{
-		while (j >= 0)
-		{
-			if (num % powB(10, j) == 0 && j != 0)
-			{
-				_putchar(48 + num / powB(10, j));
-				for (k = j; k > 0; k--)
-					_putchar(48);
-				j = -1;
-			}
-			else
-			{
-				digit1 = num / powB(10, j);
-				_putchar(digit1 + 48);
-
-				tmp2 = num;
-				num -= powB(10, j) * digit1;
-				if (numLength(tmp2) - numLength(num) == 2)
-				{
-					_putchar(48);
-					j--;
-				}
-				j--;
-			}
-		}
-	}
-}
-
-
-
-
-/**
- * print_number - prints out any integer with only _putchar
- * @n: integer to be printed
- * Return: 0
+ * return: none
  */
 void print_number(int n)
 {
+	int div, last_digit, tmp, lenght;
 
-	printnchar(n);
+	/* calcualating last digit */
+	last_digit = n % 10;
+	/* if negative print '-' and make last digit positive */
+	if (last_digit < 0)
+	{
+		_putchar('-');
+		last_digit = last_digit * (-1);
+	}
+	/* calculate the number without the last digit and make positive */
+	n = (n / 10);
+	if (n < 0)
+	{
+		n = n * (-1);
+	}
+	/* calculate the length of a number and div to divide it with */
+	lenght  = 0;
+	div = 1;
+	tmp = n;
+	while (tmp > 0)
+	{
+		lenght += 1;
+		div *= 10;
+		tmp = tmp / 10;
+	}
+	/* make div one digit shorter than lenght */
+	div = div / 10;
+	while (div >= 1)
+	{
+		_putchar(n / div + '0');
+		n = n % div;
+		div /= 10;
+	}
+	_putchar(last_digit + '0');
 }
