b7c6ce1651f63fe94421a1fc7473fbef62065b83
Ojok Simon Peter <simonpeter.oj@gmail.com>
2021-07-13 04:27:57 -0700

    Header file holberton.h

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,6 +1,10 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-void _putchar(char);
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
@@ -8,4 +12,8 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
-#endif
+int countWords(char *s);
+int endIndex(char *s, int index);
+int startIndex(char *s, int index);
+
+#endif /* HOLBERTON_H */
