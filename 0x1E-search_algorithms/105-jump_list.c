9f64ee3ac7181f66a3cc57cceeba9d7062cfe3dc
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-01-20 01:22:42 +0300

    Update 105-jump_list.c

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -2,40 +2,78 @@
 #include <math.h>
 
 /**
+ * linear_mod - function that searches for a value in an linked list
+ * using the Linear search algorithm
+ * @start_n: a pointer to the first node
+ * @start: initial index
+ * @end: final index
+ * @value: is the value to search for
+ * Return: pointer to the first node where value is located or NULL if value
+ * is not present in head or if head is NULL.
+ */
+
+listint_t *linear_mod(listint_t *start_n, size_t start, size_t end, int value)
+{
+	size_t i;
+
+	if (start_n == NULL)
+		return (NULL);
+
+	for (i = start; i <= end; i++, start_n = start_n->next)
+	{
+		printf("Value checked at index [%d] = [%d]\n", (int) i, start_n->n);
+		if (start_n->n == value)
+			return (start_n);
+	}
+	return (NULL);
+}
+
+/**
  * jump_list - function that searches for a value in a sorted list of
  * integers using the Jump search algorithm.
- * @list: A pointer to the  head of the linked list to search.
- * @size: The number of nodes in the list.
- * @value: The value to search for.
- *
- * Return: a pointer to the first node where value is located or NULL
+ * @list: pointer to the head of the list to search in
+ * @size: the number of nodes in list
+ * @value: is the value to search for
+ * Return: pointer to the first node where value is located or NULL if value
+ * is not present in head or if head is NULL.
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	size_t step, step_size;
-	listint_t *l, *r;
+	size_t start, end, block_s;
+	listint_t *end_n, *start_n;
 
-	if (list == NULL || size == 0)
+	block_s = sqrt(size);
+
+	if (list == NULL)
 		return (NULL);
 
-	step = 0;
-	step_size = sqrt(size);
-	for (l = r = list; r->index + 1 < size && r->n < value;)
+	for (end_n = list, start_n = list; end_n->index != block_s;
+	     end_n = end_n->next)
+		;
+	for (start = 0, end = block_s; end < size && end_n->next;)
 	{
-		l = r;
-		for (step += step_size; r->index < step; r = r->next)
+		printf("Value checked at index [%d] = [%d]\n", (int) end, end_n->n);
+		if (start_n->n > value && start == 0)
+			return (NULL);
+		else if (end_n->n >= value)
+			break;
+		for (end += block_s; end_n->index != end && end_n->next; end_n = end_n->next)
 		{
-			if (r->index + 1 == size)
-				break;
+			start_n = start_n->next;
+			start = end - block_s;
 		}
-		printf("Value checked at index [%ld] = [%d]\n", r->index, r->n);
 	}
-	printf("Value found between indexes [%ld] and [%ld]\n",
-	       l->index, r->index);
 
-	for (; l->index < r->index && l->n < value; l = l->next)
-		printf("Value checked at index [%ld] = [%d]\n", l->index, l->n);
-	printf("Value checked at index [%ld] = [%d]\n", l->index, l->n);
+	if (end >= size - 1)
+	{
+		start_n = start_n->next;
+		printf("Value checked at index [%d] = [%d]\n", (int) (size - 1), end_n->n);
+		printf("Value found between indexes [%d] and [%d]\n", (int) start,
+		       (int) (size - 1));
+		return (linear_mod(start_n, start, size - 1, value));
+	}
 
-	return (l->n == value ? l : NULL);
+	printf("Value found between indexes [%d] and [%d]\n", (int) (end - block_s),
+	       (int) (end));
+	return (linear_mod(start_n, start, end, value));
 }
